321bb7d3de9e2a77a8fe9cd76649c3783ad91f9c
RenzoBTL30 <90786322+RenzoBTL30@users.noreply.github.com>
2024-07-01 19:07:25 -0500

    nuevo cambio 3

--- src/app/service-ia.service.ts
+++ src/app/service-ia.service.ts
@@ -1,9 +1,33 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ServiceIAService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  apiUrl:any = 'http://127.0.0.1:8000/preguntas';
+
+  private httpHeaders = new HttpHeaders(
+    {'Content-Type': 'application/json'}
+  );
+
+  getData(): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}`, {headers: this.httpHeaders});
+  }
+
+  postData(textopdf:string, num_questions:number, habilidad:string): Observable<any> {
+    return this.http.post<any>(`${this.apiUrl}`, 
+    {
+      textopdf:textopdf,
+      num_questions:num_questions,
+      habilidad:habilidad
+    }, 
+    {headers: this.httpHeaders});
+  }
+
+  
 }
